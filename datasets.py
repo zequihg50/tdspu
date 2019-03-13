a1c8fbf86385ac6ddbe9d7f5e2f3728d54aa1a58
zequihg50 <zequi3000@hotmail.com>
2019-03-13 19:33:18 +0100

    Including fx variables into ncmls. There are nc which are the same fx variable but with different r*i*p*. Are variables of different realms merged under the same variable name?

--- datasets.py
+++ datasets.py
@@ -1,58 +1,53 @@
 #!/usr/bin/env python
 
-import sys, os
-import netCDF4
+# Usage: python datasets.py --root $(pwd)/data/cmip5/output1/NOAA-GFDL/GFDL-ESM2M/historical --dest $(pwd) --name test.ncml
 
-from collections import defaultdict
+import sys, os
+import argparse
+import netCDF4
+import itertools
+
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-class Dataset(object):
-
-    def __init__(self, root):
-        self.root = root
-
-    # Write this dataset to a ncml file with name 'name' in path 'dest'
-    def to_ncml(self, name, dest, template='dataset.ncml.j2'):
-        def get_files(path):
-            files = []
-            for dirpath, dirnames, filenames in os.walk(path):
-                files.extend([os.path.join(path, dirpath, f) for f in filenames])
-            files.sort()
-            return files
-
-        def get_ncoords(dataset, dimension='time'):
-            return dataset.dimensions[dimension].size
-
-        files = get_files(self.root)
-        taggs = defaultdict(list)
-
-        # Some datasets have directory but are empty
-        if len(files) < 1:
-            return
-
-        for path in get_files(self.root):
-            file = os.path.basename(path)
-            var = '_'.join(file.split("_")[0:2]) # a variable is identified by (name,realm)
-
-            dataset = netCDF4.Dataset(path)
-            taggs[var].append( (path, get_ncoords(dataset)) )
-            dataset.close()
-
-        env = Environment(loader=FileSystemLoader('.'), autoescape=select_autoescape(['xml']))
-        template = env.get_template(template)
-
-        filepath = os.path.join(dest, name)
-        with open(filepath, 'w+') as fh:
-            fh.write(template.render(taggs=taggs))
-
 if __name__ == '__main__':
-    def get_directories(root):
-        for dirpath, dirnames, filenames in os.walk(root):
-            if not dirnames:
-                yield os.path.split(dirpath)[0] # ignore versions
+    def get_files(path):
+        for dirpath, dirnames, filenames in os.walk(path):
+            if filenames:
+                yield map(lambda f: os.path.join(dirpath, f), filenames)
 
-    root = sys.argv[1]
-    dest = os.path.join(os.path.dirname(__file__), 'datasets')
-    for d in get_directories(root):
-        ncml_name = d.replace(root,'').replace('/', '_') + '.ncml'
-        Dataset(d).to_ncml(ncml_name, dest)
+    def ncoords(file, dimension):
+        dataset = netCDF4.Dataset(file)
+        size = dataset.dimensions[dimension].size
+        dataset.close()
+
+        return size
+
+    # Arguments
+    parser = argparse.ArgumentParser(description='Create ncml for files in directory.')
+    parser.add_argument('--root', dest='root', type=str, help='Path containing files')
+    parser.add_argument('--name', dest='name', type=str, help='ncml filename')
+    parser.add_argument('--dest', dest='dest', type=str, help='Path to ncml')
+    args = parser.parse_args()
+
+    # Jinja
+    env = Environment(loader=FileSystemLoader('.'), autoescape=select_autoescape(['xml']))
+    env.globals['ncoords'] = ncoords
+    template = env.get_template('dataset.ncml.j2')
+
+    # Generate aggregations
+    files = list(itertools.chain.from_iterable(get_files(args.root)))
+    files.sort()
+    groups = []
+    for k, g in itertools.groupby(files, lambda file: os.path.basename(file).split('_')[0:2]):
+        groups.append((k, list(g))) # g is an iterator, we want a list
+
+    # Write ncml
+    if(args.name is None):
+        parts = args.root.split('/')[::-1][:5]
+        args.name = '-'.join(parts) + '.ncml'
+
+    if(args.dest is None):
+        args.dest = '/tmp'
+
+    with open(os.path.join(args.dest, args.name), 'w+') as fh:
+        fh.write(template.render(groups=groups))
