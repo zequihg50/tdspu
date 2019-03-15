12ac18efa104323b4927cf06505632a74b6409f8
zequihg50 <zequi3000@hotmail.com>
2019-03-15 14:03:30 +0100

    Primer test creando un catalogo que incluye el dataset virtual (ncml) y datasetscan para acceso a las variables. Tengo que cambiar los datasetscan por nested datasets, a poder ser cada uno en su propio catalogo, para que no aparezcan mil ficheros en una sola pantalla. El comando es `python datasets.py --root $(pwd)/data/cmip5/output1/NOAA-GFDL/GFDL-ESM2M/historical --dest datasets`

--- datasets.py
+++ datasets.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 
-# Usage: python datasets.py --root $(pwd)/data/cmip5/output1/NOAA-GFDL/GFDL-ESM2M/historical --dest $(pwd) --name test.ncml
+# Usage:
+# python datasets.py --root $(pwd)/data/cmip5/output1/NOAA-GFDL/GFDL-ESM2M/historical --dest $(pwd) --name test.ncml
+# find $(pwd)/data -maxdepth 5 -mindepth 5 -type d | xargs -I{} -n1 python datasets.py --root {} --dest datasets/
 
 import sys, os
 import argparse
 import netCDF4
 import itertools
+import xml.etree.ElementTree as ET
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
@@ -13,7 +16,9 @@ if __name__ == '__main__':
     def get_files(path):
         for dirpath, dirnames, filenames in os.walk(path):
             if filenames:
-                yield map(lambda f: os.path.join(dirpath, f), filenames)
+                # exclude seaIce
+                if dirpath.split('/')[::-1][3] != 'seaIce':
+                    yield map(lambda f: os.path.join(dirpath, f), filenames)
 
     def ncoords(file, dimension):
         dataset = netCDF4.Dataset(file)
@@ -32,7 +37,7 @@ if __name__ == '__main__':
     # Jinja
     env = Environment(loader=FileSystemLoader('.'), autoescape=select_autoescape(['xml']))
     env.globals['ncoords'] = ncoords
-    template = env.get_template('dataset.ncml.j2')
+    dataset_template = env.get_template('dataset.ncml.j2')
 
     # Generate aggregations
     files = list(itertools.chain.from_iterable(get_files(args.root)))
@@ -50,4 +55,40 @@ if __name__ == '__main__':
         args.dest = '/tmp'
 
     with open(os.path.join(args.dest, args.name), 'w+') as fh:
-        fh.write(template.render(groups=groups))
+        fh.write(dataset_template.render(groups=groups))
+
+    # Open the catalog and append the dataset
+    ET.register_namespace("", "http://www.unidata.ucar.edu/namespaces/thredds/InvCatalog/v1.0")
+    tree = ET.parse('catalog-template.xml')
+    root = tree.getroot()
+    dataset_root = root.find('{http://www.unidata.ucar.edu/namespaces/thredds/InvCatalog/v1.0}datasetRoot')
+
+    print(args.name)
+    nested_dataset = ET.SubElement(root, 'dataset')
+    nested_dataset.attrib = {
+        'name': args.name.split('.')[0],
+        'ID': args.name.split('.')[0],
+    }
+
+    access = ET.SubElement(nested_dataset, 'access')
+    access.attrib = {
+        'serviceName': 'virtual',
+        'urlPath': os.path.join(dataset_root.attrib['path'], 'datasets', args.name),
+        'dataFormat': 'NcML'
+    }
+
+    for variable, files in groups:
+        path = os.path.join(dataset_root.attrib['path'], '-'.join(variable))
+
+        dataset = ET.SubElement(root, 'datasetScan')
+        dataset.attrib = {
+            'name': ' '.join(variable),
+            'path': '-'.join(variable),
+            'location': os.path.dirname(files[0])
+        }
+
+        filter = ET.SubElement(dataset, 'filter')
+        filter_include = ET.SubElement(filter, 'include')
+        filter_include.attrib = { 'wildcard': '_'.join(variable) + '*' }
+
+    tree.write('catalog.xml')
