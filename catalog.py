d3060ba18c29292ea42efe3635fcd6d9f42a8ef7
zequihg50 <zequi3000@hotmail.com>
2019-03-25 19:16:53 +0100

    ncml.py is now constructed using OOP and catalogs are written to stdout instead of file

--- catalog.py
+++ catalog.py
@@ -11,16 +11,16 @@ if __name__ == '__main__':
     parser.add_argument('--dataset', dest='dataset', type=str, help='ZFS dataset path')
     args = parser.parse_args()
 
-    catalogs_path = os.path.join(args.dataset, 'catalogs')
     ncmls_path = os.path.join(args.dataset, 'ncmls')
     ncmls = os.listdir(ncmls_path)
 
     # Jinja
     env = Environment(loader=FileSystemLoader('.'), autoescape=select_autoescape(['xml']))
     template = env.get_template('catalog.xml.j2')
+    params = {
+        'name': os.path.basename(args.dataset),
+        'ncmls': map(lambda n: os.path.splitext(n)[0], ncmls),
+        'dataset': args.dataset
+    }
 
-    with open(os.path.join(args.dataset, 'catalogs', 'catalog.xml'), 'w+') as fh:
-        fh.write(template.render(
-            name=os.path.basename(args.dataset),
-            ncmls=map(lambda n: os.path.splitext(n)[0], ncmls),
-            dataset=args.dataset))
+    print(template.render(**params))
--- ncml.py
+++ ncml.py
@@ -2,7 +2,7 @@
 
 # Conventions:
 # 1 - The parameter is the ZFS dataset path
-# 2 - Every ZFS dataset contains the directories: data, ncmls, catalogs
+# 2 - Every ZFS dataset contains the directories: data, ncmls
 
 import sys, os
 import argparse
@@ -11,23 +11,41 @@ import itertools
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-def aggregate(files):
-    aggregations = []
-    files.sort() # itertools.groupby requires order
-    for _, g in itertools.groupby(files, lambda file: os.path.basename(file).split('_')[0:2]):
-        aggregations.append(list(g)) # g is an iterator, we want a list
+class NcmlGenerator(object):
+    def __init__(self, files, template='default.ncml.j2'):
+        self.files = files
+        self.template = template
 
-    return aggregations
+    def aggregate(self):
+        return self.files
 
-def ncoords(file, dimension):
-    dataset = netCDF4.Dataset(file)
-    size = dataset.dimensions[dimension].size
-    dataset.close()
+    def generate(self):
+        env = Environment(loader=FileSystemLoader('.'), autoescape=select_autoescape(['xml']))
+        env.globals['ncoords'] = self.ncoords
+        template = env.get_template(self.template)
+        params = { 'aggregations': self.aggregate() }
 
-    return size
+        return template.render(**params)
 
-# class ncml_aggregator(object)
-# def ncml_aggregator(files, dataset, aggregate=aggregate, template='ncml.j2')
+    # Required in the jinja env to extract ncoords
+    def ncoords(self, file, dimension):
+        dataset = netCDF4.Dataset(file)
+        size = dataset.dimensions[dimension].size
+        dataset.close()
+
+        return size
+
+class EsgfNcmlGenerator(NcmlGenerator):
+    def __init__(self):
+        super().__init__(files, 'esgf.ncml.j2')
+
+    def aggregate(self):
+        aggregations = []
+        self.files.sort() # itertools.groupby requires order
+        for _, g in itertools.groupby(self.files, lambda file: os.path.basename(file).split('_')[0:2]):
+            aggregations.append(list(g)) # g is an iterator, we want a list
+
+        return aggregations
 
 if __name__ == '__main__':
     # Arguments
@@ -35,19 +53,14 @@ if __name__ == '__main__':
     parser.add_argument('--dataset', dest='dataset', type=str, help='ZFS dataset path')
     args = parser.parse_args()
 
-    # Generate aggregations
     files =  sys.stdin.read().splitlines()
-    aggregations = aggregate(files)
-
-    # Jinja
-    env = Environment(loader=FileSystemLoader('.'), autoescape=select_autoescape(['xml']))
-    env.globals['ncoords'] = ncoords
-    template = env.get_template('dataset.ncml.j2')
 
     # Generate ncml
     root = os.path.join(args.dataset, 'data')
     ncml = '_'.join(files[0].replace(root, '')[1:].split('/')[:5])
     file = ncml + '.ncml'
 
+    generator = EsgfNcmlGenerator()
+
     with open(os.path.join(args.dataset, 'ncmls', file), 'w+') as fh:
-        fh.write(template.render(aggregations=aggregations))
+        fh.write(generator.generate())
